e42c977a39daa80d0ea381a28b3046cf8b480a7b
Victor Garcia <victor.garciagutierrez.itcm@gmail.com>
2024-04-06 15:09:36 -0600

    (Id ####) Se agrega la funcionalidad del slide, la busqueda de gifs con la api y la muestra en pantalla de los titulos.

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -7,10 +8,24 @@ import { Component } from '@angular/core';
     <input type="text"
       class="form-control"
       placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
     >
   `,
   styleUrl: './search-box.component.css'
 })
 export class SearchBoxComponent {
 
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService){
+
+  }
+
+  searchTag(  ){
+    const tagInput:string = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag( tagInput );
+    this.tagInput.nativeElement.value = '';
+  }
 }
